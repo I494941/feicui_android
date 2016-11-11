67937beae3b578f857ded0ce41a124c2bfcef6ae
fishcy521 <fishcy521@163.com>
2016-11-11 11:17:09 +0800

    listview1问题解决 asynctask和handler改写处理

--- expressage/src/main/java/com/feicuiedu/android/expressage/MainActivity.java
+++ expressage/src/main/java/com/feicuiedu/android/expressage/MainActivity.java
@@ -2,7 +2,10 @@ package com.feicuiedu.android.expressage;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +13,24 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,12 +38,91 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     private Context context;
 
     private EditText edOrder;
     private Spinner spCompany;
     private Button btnQuery;
     private ListView lvData;
+
+    private  String orderNo;
+
+    private List<Map<String,String>> lstData = new ArrayList<>();
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            if (msg.what == 1) {
+                String strJson = (String) msg.obj;
+                Log.d(TAG, "handleMessage: "+strJson);
+
+                try {
+                    JSONObject jsonObject = new JSONObject(strJson);
+                    JSONArray array= jsonObject.getJSONArray("data");
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject obj = array.getJSONObject(i);
+                        String time = obj.getString("time");
+                        String ftime = obj.getString("ftime");
+                        String context = obj.getString("context");
+
+                        Map<String,String> tmpMap = new HashMap<>();
+                        tmpMap.put("time", time);
+                        tmpMap.put("ftime", ftime);
+                        tmpMap.put("context", context);
+
+                        lstData.add(tmpMap);
+
+                        lvData.setAdapter(lvAdapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private ListAdapter lvAdapter = new BaseAdapter() {
+
+        @Override
+        public int getCount() {
+            return lstData.size();
+        }
+
+        @Override
+        public Map<String,String> getItem(int position) {
+            return lstData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater lif = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = lif.inflate(R.layout.listview_item_layout, null);
+
+            TextView tvTime = (TextView) convertView.findViewById(R.id.tv_time);
+            TextView tvFTime = (TextView) convertView.findViewById(R.id.tv_ftime);
+            TextView tvContent = (TextView) convertView.findViewById(R.id.tv_content);
+
+            Map<String,String> map = getItem(position);
+            String time = map.get("time");
+            String ftime = map.get("ftime");
+            String context = map.get("context");
+
+            tvTime.setText(time);
+            tvFTime.setText(ftime);
+            tvContent.setText(context);
+
+
+            return convertView;
+        }
+    };
 
     private List<Map<String, String>> lstCompany = new ArrayList<>();
 
@@ -68,17 +163,69 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-            String orderNo = String.valueOf(edOrder.getText());
+            orderNo = String.valueOf(edOrder.getText());
 
-            MyAsyncTask mat = new MyAsyncTask(lvData,context);
-            mat.execute(orderNo,selectedCompany);
+            /*MyAsyncTask mat = new MyAsyncTask(lvData,context);
+            mat.execute(orderNo,selectedCompany);*/
+
+            new Thread() {
+                @Override
+                public void run() {
+                    String company = selectedCompany;
+
+                    String strUrl = "http://www.kuaidi100.com/query";
+                    String strParams = "type="+company+"&postid="+orderNo;
+
+                    String result = null;
+                    URL url = null;
+                    try {
+                        url = new URL(strUrl);
+                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                        httpURLConnection.setConnectTimeout(5000);
+
+                        httpURLConnection.setDoOutput(true);
+                        httpURLConnection.setDoInput(true);
+
+                        httpURLConnection.setRequestMethod("POST");
+                        // httpURLConnection.setRequestMethod("POST");
+
+                        // 获取输出流
+                        OutputStream out = httpURLConnection.getOutputStream();
+                        out.write(strParams.getBytes());
+                        out.flush();
+
+                        if (httpURLConnection.getResponseCode() == 200) {
+
+                            InputStream is = httpURLConnection.getInputStream();
+
+                            byte[] data = read(is);
+                            result = new String(data, "UTF-8");
+                        }
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (ProtocolException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    Message message = new Message();
+                    message.obj = result;
+                    message.what = 1;
+
+                    handler.sendMessage(message);
+                }
+            }.start();
+
         }
     };
     private AdapterView.OnItemSelectedListener spOisl = new AdapterView.OnItemSelectedListener() {
 
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            Map<String,String> map = lstCompany.get(position);
+            Map<String, String> map = lstCompany.get(position);
             String key = "";
             for (Map.Entry<String, String> entry : map.entrySet()) {
                 key = entry.getKey();
@@ -171,5 +318,17 @@ public class MainActivity extends AppCompatActivity {
         result.add(map);
 
         return result;
+    }
+
+    // 从流中读取数据
+    public byte[] read(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        inStream.close();
+        return outStream.toByteArray();
     }
 }
--- listviewdemo1/src/main/java/com/feicuiedu/android/listviewdemo1/MultipleActivity.java
+++ listviewdemo1/src/main/java/com/feicuiedu/android/listviewdemo1/MultipleActivity.java
@@ -33,22 +33,62 @@ public class MultipleActivity extends AppCompatActivity {
     // 布局加载器
     private LayoutInflater lif;
 
-    private List<Map<String,Object >> listData;
+    private List<Map<String, Object>> listData;
 
     private List<String> listDataSelected = new ArrayList<>();
 
+    // 记录Listview中所有checkbox的选中状态
+    private Map<String,Boolean> selectedMap = new HashMap<>();
+
+    // 布局中item中checkbox的oncheckedchange事件
     private CompoundButton.OnCheckedChangeListener itemOccl = new CompoundButton.OnCheckedChangeListener() {
 
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
+
             String id = (String) buttonView.getTag();
-            if (isChecked) {
-                listDataSelected.add(id);
+
+            selectedMap.put(id,isChecked);
+
+
+            int selecedSize = 0;
+
+            for (Map.Entry<String,Boolean> entry:selectedMap.entrySet()) {
+
+                if (entry.getValue()) {
+                    selecedSize ++;
+                }
+            }
+
+            if (selecedSize == listData.size()) {
+
+                chkAll.setChecked(true);
             }
             else {
+                chkAll.setChecked(false);
+            }
+            /*for (int i = 0; i < listData.size(); i++) {
+
+                Map<String, Object> tmpMap = listData.get(i);
+
+                if (tmpMap.get("id" + i).equals(id)) {
+                    tmpMap.put("isChecked", isChecked);
+                }
+            }
+
+
+            if (isChecked) {
+                listDataSelected.add(id);
+            } else {
                 listDataSelected.remove(id);
             }
+
+                Log.d(">>>>>>>>>>>>>>", "listDataSelected.size="+listDataSelected.size()+"||listData.size="+listData.size());
+            if (listDataSelected.size() == listData.size() - 1) {
+
+                chkAll.setChecked(isChecked);
+            }*/
         }
     };
 
@@ -62,7 +102,7 @@ public class MultipleActivity extends AppCompatActivity {
         }
 
         @Override
-        public Map<String,Object > getItem(int position) {
+        public Map<String, Object> getItem(int position) {
             return listData.get(position);
         }
 
@@ -78,7 +118,7 @@ public class MultipleActivity extends AppCompatActivity {
 
             if (convertView == null) {
 
-                convertView = lif.inflate(R.layout.activity_multiple_item,null);
+                convertView = lif.inflate(R.layout.activity_multiple_item, null);
                 vh = new ViewHolder();
                 vh.itemChk = (CheckBox) convertView.findViewById(R.id.item_chk);
                 vh.itemIv = (ImageView) convertView.findViewById(R.id.item_iv);
@@ -86,23 +126,23 @@ public class MultipleActivity extends AppCompatActivity {
                 vh.itemTvContent = (TextView) convertView.findViewById(R.id.item_tv_content);
                 vh.itemTvEnd = (TextView) convertView.findViewById(R.id.item_tv_end);
                 convertView.setTag(vh);
-            }
-            else {
+            } else {
                 vh = (ViewHolder) convertView.getTag();
             }
 
-            Map<String,Object> map = getItem(position);
+            Map<String, Object> map = getItem(position);
 
-            String id = (String) map.get("id"+position);
+            String id = (String) map.get("id" + position);
             String name = (String) map.get("name");
             String content = (String) map.get("content");
             String end = (String) map.get("end");
+            vh.itemChk.setTag(id);
+            vh.itemChk.setOnCheckedChangeListener(itemOccl);
             Boolean isChecked = (Boolean) map.get("isChecked");
 
-            vh.itemChk.setChecked(isChecked);
+            vh.itemChk.setChecked(selectedMap.get(id));
 
-            vh.itemChk.setOnCheckedChangeListener(itemOccl);
-            vh.itemChk.setTag(id);
+
             vh.itemTvName.setText(name);
             vh.itemTvContent.setText(content);
             vh.itemTvEnd.setText(end);
@@ -112,13 +152,32 @@ public class MultipleActivity extends AppCompatActivity {
         }
     };
 
-    private CompoundButton.OnCheckedChangeListener occl = new CompoundButton.OnCheckedChangeListener() {
+    // 全选的那个checkbox 的oncheckedchange事件
+    /*private CompoundButton.OnCheckedChangeListener occl = new CompoundButton.OnCheckedChangeListener() {
 
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
             for (Map<String,Object> map :listData) {
                 map.put("isChecked",isChecked);
+            }
+
+            ba.notifyDataSetChanged();
+        }
+    };*/
+
+    private View.OnClickListener chkAllocl = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            CheckBox cb = (CheckBox) v;
+            /*for (Map<String, Object> map : listData) {
+                map.put("isChecked", cb.isChecked());
+            }*/
+
+            for (Map.Entry<String,Boolean> entry:selectedMap.entrySet()) {
+                selectedMap.put(entry.getKey(),cb.isChecked());
             }
 
             ba.notifyDataSetChanged();
@@ -130,28 +189,18 @@ public class MultipleActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             if (listDataSelected == null || listDataSelected.isEmpty()) {
-                Toast.makeText(context,"您没有勾选任意一笔资料",Toast.LENGTH_SHORT).show();
-            }
-            else {
+                Toast.makeText(context, "您没有勾选任意一笔资料", Toast.LENGTH_SHORT).show();
+            } else {
 
                 StringBuilder sb = new StringBuilder();
-                for (String id:listDataSelected) {
+                for (String id : listDataSelected) {
                     sb.append(id).append(",");
                 }
 
-                Toast.makeText(context,"您勾选的资料id为:"+sb,Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "您勾选的资料id为:" + sb, Toast.LENGTH_SHORT).show();
             }
         }
     };
-
-    private class ViewHolder {
-        CheckBox itemChk;
-        ImageView itemIv;
-        TextView itemTvName;
-        TextView itemTvContent;
-        TextView itemTvEnd;
-    }
-
     private AdapterView.OnItemClickListener oicl = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -175,7 +224,8 @@ public class MultipleActivity extends AppCompatActivity {
         btnShow = (Button) findViewById(R.id.btn_show);
 
         btnShow.setOnClickListener(showOcl);
-        chkAll.setOnCheckedChangeListener(occl);
+        // chkAll.setOnCheckedChangeListener(occl);
+        chkAll.setOnClickListener(chkAllocl);
         // 初始化数据源
         listData = getData();
 
@@ -184,23 +234,32 @@ public class MultipleActivity extends AppCompatActivity {
 
     }
 
-    private List<Map<String ,Object >> getData() {
+    private List<Map<String, Object>> getData() {
 
-        List<Map<String,Object>> result = new ArrayList<>();
+        List<Map<String, Object>> result = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
-            Map<String,Object> map = new HashMap<>();
-            map.put("id"+i,"key"+i);
-            map.put("name","我是name"+i);
-            map.put("content","我的内容是"+i);
-            if (i%2 == 0) {
-                map.put("end","系统进程");
+            Map<String, Object> map = new HashMap<>();
+            map.put("id" + i, "key" + i);
+            map.put("name", "我是name" + i);
+            map.put("content", "我的内容是" + i);
+            if (i % 2 == 0) {
+                map.put("end", "系统进程");
+            } else {
+                map.put("end", "用户进程");
             }
-            else {
-                map.put("end","用户进程");
-            }
-            map.put("isChecked",false);
+            map.put("isChecked", false);
+
+            selectedMap.put("key" + i, false);
             result.add(map);
         }
         return result;
+    }
+
+    private class ViewHolder {
+        CheckBox itemChk;
+        ImageView itemIv;
+        TextView itemTvName;
+        TextView itemTvContent;
+        TextView itemTvEnd;
     }
 }
